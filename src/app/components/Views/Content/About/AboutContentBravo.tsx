d4b08b8a15f8c19bcedc267edbffc4f7ee2e8619
bacm6 <bacm640@gmail.com>
2024-06-17 06:54:43 -0500

    bugfix: build errors with linting resolved.

--- src/app/components/Views/Content/About/AboutContentBravo.tsx
+++ src/app/components/Views/Content/About/AboutContentBravo.tsx
@@ -16,13 +16,13 @@ export const AboutContentBravo = observer(() : React.JSX.Element => {
 
                 <h3>What sort of positions have I held?</h3>
 
-                <p>I've been in roles ranging from junior software engineer, mid-level software engineer, and currently I'm acting in a senior software engineer capacity.</p>
+                <p>I&apos;ve been in roles ranging from junior software engineer, mid-level software engineer, and currently I&apos;m acting in a senior software engineer capacity.</p>
 
                 <h3>What does leadership look like to me?</h3>
 
-                <p>So far, I've seen a mix. It's intense ownership over a product, as well as keen understanding of your team's capacity.</p>
-                <p>It's also being able to commit to realistic deadlines and understanding how to communicate to relevant stakeholders.</p>
-                <p>TLDR; it's not all about coding...</p>
+                <p>So far, I&apos;ve seen a mix. It&apos;s intense ownership over a product, as well as keen understanding of your team&apos;s capacity.</p>
+                <p>It&apos;s also being able to commit to realistic deadlines and understanding how to communicate to relevant stakeholders.</p>
+                <p>TLDR; it&apos;s not all about coding...</p>
 
             </div>
 
